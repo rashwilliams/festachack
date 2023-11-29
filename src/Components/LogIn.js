d3c170eb8f48083a8962e89e003c0c887bedaa7a
rashwilliams <rashidi.william@protonmail.com>
2023-11-29 10:54:20 +0100

    first commit

---
+++ src/Components/LogIn.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from "react";
+import { Button, Form } from "react-bootsratp";
+
+const Login = () => {
+  return (
+    <>
+      <Fragment>
+        <Container className={styles.container6}>
+          <Row>
+            <Col></Col>
+            <Col>
+              <Button className={styles.button2} variant="secondary" size="lg">
+                Log In
+              </Button>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+        <Row>
+          <Col></Col>
+          <Col>
+            <Container>
+              <Form>
+                <Form.Label>Username</Form.Label>
+                <Form.Control placeholder="Usernamel" />
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control placeholder="Enter Email" />
+                <Form.Label>Password</Form.Label>
+                <Form.Control placeholder="Enter password" type="password" />
+                <div>
+                  <Button type="submit"> Login </Button>{" "}
+                </div>
+              </Form>
+            </Container>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Fragment>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/Components/Register.js
@@ -0,0 +1,55 @@
+import React, { Fragment } from "react";
+import { Button, Form } from "react-bootsratp";
+
+const Register = () => {
+  return (
+    <>
+      <Fragment>
+        <Container className={styles.container}>
+          <Row>
+            <Col></Col>
+            <Col>
+              {/* <div>
+                  <h1>Sign Up</h1>
+                </div> */}
+              <Button className={styles.button2} variant="secondary" size="lg">
+                Sign Up
+              </Button>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+
+        <Row>
+          <Col></Col>
+          <Col>
+            <Container>
+              <Form onSubmit>
+                <Form.Label>Name</Form.Label>
+                <Form.Control placeholder="Enter First Name" />
+                <Form.Label>Name</Form.Label>
+                <Form.Control placeholder="Enter Last Name" />
+                <Form.Label>Country</Form.Label>
+                <Form.Control placeholder="Enter Country from the list" />
+                <Form.Label>Phonenumber</Form.Label>
+                <Form.Control placeholder="Enter Phonenumber" />
+                <Form.Label>username</Form.Label>
+                <Form.Control placeholder="Enter Username" />
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control placeholder="Enter Email" />
+                <Form.Label>Password</Form.Label>
+                <Form.Control placeholder="Enter Password" type="password" />
+                <div>
+                  <Button type="submit"></Button>
+                </div>
+              </Form>
+            </Container>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Fragment>
+    </>
+  );
+};
+
+export default Register;
